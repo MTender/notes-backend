88a4e922b364718ce4c601e8bf09e431d76fd4d8
Märt Tender <mart.tender@gmail.com>
2023-10-30 19:50:03 +0200

    uibuild

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import express from "express";
-import cors from "cors";
-import Note from "./models/note.js";
+import express from 'express'
+import cors from 'cors'
+import Note from './models/note.js'
 
 const app = express()
 
